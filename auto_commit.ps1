c86c7240714ad87ab659fea33156481f74f911f5
waynehet <waynehet@hotmail.com>
2018-04-16 22:33:37 -0400

    Updated bookmarks

--- auto_commit.ps1
+++ auto_commit.ps1
@@ -1,4 +1,5 @@
 Write-Host "Copying bookmarks file"
 copy-item "C:\Users\Wayne\AppData\Local\Google\Chrome\User Data\Profile 2\Bookmarks" -destination "C:\Users\Wayne\Documents\GitHub\classz.github.io\_data\Bookmarks.json"
 git commit -m "Updated bookmarks file"
+git pull
 git push
