8aa2f57af11720be78c607872cf437cb4557641c
waynehet <waynehet@gmail.com>
2023-01-22 22:14:14 -0500

    updated bookmark commit script

--- auto_commit.ps1
+++ auto_commit.ps1
@@ -1,9 +1,8 @@
 Clear-Host
 Write-Host "Copying bookmarks file"
-# copy-item "C:\Users\Wayne\AppData\Local\Google\Chrome\User Data\Profile 2\Bookmarks" -destination "C:\Users\Wayne\Documents\GitHub\classz.github.io\_data\Bookmarks.json"
 copy-item "C:\Users\wayne\AppData\Local\Vivaldi\User Data\Default\Bookmarks" -Destination "C:\Users\wayne\OneDrive\Code\classz.github.io\_data\Bookmarks.json"
 git status
-git add "C:\Users\Wayne\Documents\GitHub\classz.github.io\_data\Bookmarks.json"
+git add "C:\Users\wayne\OneDrive\Code\classz.github.io\_data\Bookmarks.json"
 $commit_msg = "Updated bookmarks file " + (get-date -f MM-dd-yyyy_HH_mm_ss)
 git commit -m $commit_msg
 git pull
